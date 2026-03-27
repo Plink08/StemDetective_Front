42625ab9a5d26847a3a9fa88dca4d37da5a3604a
Plink08 <lennvanderlake@gmail.com>
2026-03-27 16:42:42 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,18 @@
+{
+  "name": "StemDetective",
+  "short_name": "StemDetective",
+  "start_url": ".",
+  "display": "standalone",
+  "icons": [
+    {
+      "src": "small_icon.ico",
+      "sizes": "192x192",
+      "type": "image/x-icon"
+    },
+    {
+      "src": "big_icon.ico",
+      "sizes": "512x512",
+      "type": "image/x-icon"
+    }
+  ]
+}
